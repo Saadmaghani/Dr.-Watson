67f221c166f9a368311f59929ccef41590baf03d
Saad Ghani <saadhku2@gmail.com>
2017-06-17 16:12:35 +0500

    getting better and better
    Changed a lot. Made my code a lot better by adding a lambda and using a
    method for error handling. :+1:
    still havent started on algorithms. next thing to do is to seperate code
    into another file and clean this file. THEN i can start on algorithms
    :smile:

--- drWatson.rb
+++ drWatson.rb
@@ -7,7 +7,7 @@ class Weapons
 		@name = name
 		@owned_by = owned_by
 		@@count +=1
-		$all_weapons[name]=self
+		$all_weapons[name.to_sym]=self
 	end
 
 	def self.count
@@ -24,7 +24,7 @@ class Suspects
 		@name = name
 		@owned_by = owned_by
 		@@count +=1
-		$all_suspects[name]=self
+		$all_suspects[name.to_sym]=self
 	end
 
 	def self.count
@@ -41,7 +41,7 @@ class Rooms
 		@name = name
 		@owned_by = owned_by
 		@@count+=1
-		$all_rooms[name]=self
+		$all_rooms[name.to_sym]=self
 	end
 
 	def self.count
@@ -56,84 +56,97 @@ class Players
 		@name = name
 		@total_cards = total_cards
 		@order = order
-		$all_players[name] = self
+		$all_players[name.to_sym] = self
 	end
 end
 
 class Suggestions
 	$all_suggestions={}
 	@@suggestID=0
-	attr_reader :suggest_by, :suspect, :weapon, :room, :disproved_by, :card_shown
-	def initialize(suggest_by, suspect, weapon, room, disproved_by=nil, card_shown=nil) #nil if not disproved by anyone or card not shown to the user
-		@suggest_by = suggest_by
-		@suspect = suspect
-		@weapon = weapon
-		@room = room
-		@disproved_by = disproved_by
-		@card_shown = card_shown
+	attr_accessor :suggest_by, :suspect, :weapon, :room, :disproved_by, :card_shown
+	
+	def initialize()
+		@suggest_by = ""
+		@suspect = ""
+		@weapon = ""
+		@room = ""
+		@disproved_by = ""
+		@card_shown = ""
 		@@suggestID+=1
 		$all_suggestions[@@suggestID]=self
 	end
 end
 
 #hard coded all cards for now
-Weapons.new("candlestick"); Weapons.new("revolver"); Weapons.new("rope"); Weapons.new("wrench"); Weapons.new("lead pipe"); Weapons.new("knife")
+Weapons.new("candlestick"); Weapons.new("revolver"); Weapons.new("rope"); Weapons.new("wrench"); Weapons.new("pipe"); Weapons.new("knife")
 Suspects.new("white"); Suspects.new("peacock"); Suspects.new("scarlet"); Suspects.new("mustard"); Suspects.new("green"); Suspects.new("plum")
-Rooms.new("study"); Rooms.new("library"); Rooms.new("conservatory"); Rooms.new("hall"); Rooms.new("kitchen"); Rooms.new("ballroom"); Rooms.new("dining room"); Rooms.new("lounge");Rooms.new("billiard room")
+Rooms.new("study"); Rooms.new("library"); Rooms.new("conservatory"); Rooms.new("hall"); Rooms.new("kitchen"); Rooms.new("ballroom"); Rooms.new("dining"); Rooms.new("lounge");Rooms.new("billiard")
 #not sure if all_cards needed but why not
-#all_cards = [$all_suspects, $all_weapons, $all_rooms]
+$all_cards = [$all_suspects, $all_weapons, $all_rooms].reduce &:merge
+puts $all_cards
 
-#going to handle errors from the input. not complete
-def error_handler(suggest)
-	while suggest.length < 4
-		puts "re enter suggest. not of valid format"
-		suggest = gets.chomp.split
+#going to handle errors from the input
+def error_handler(md_array=nil, card=nil)
+	err = true
+	while err==true
+		err = yield(md_array, card)
 	end
-
-	err = false
-	new_suggest = []
-	until err
-		if $all_players.has_key?(suggest[0]) 
-			new_suggest[0] = suggest[0] 
-		else
-			puts "player by not found"
-			suggest[0] = gets.chomp
-			next
-		end
-
-		for i in 1..3
-			card = suggest[i]
-			found = false
-			if $all_weapons.has_key?(card)
-				found = true
-				new_suggest[2] = card
-			elsif $all_rooms.has_key?(card)
-				found = true
-				new_suggest[3] = card
-			elsif $all_suspects.has_key?(card)
-				found = true
-				new_suggest[1] = card
-			end
-			if !found
-				puts "{card} not found"
-				suggest[i] = gets.chomp
-				next 
-			end
-		end
-	end	
+	return err
 end
 
+$finder = lambda {|md_array, card|
+	found = false
+	md_array.each{|part|
+		if part[0].has_key?(card.to_sym)
+			part[1].replace card
+			found = true
+			return false
+		end
+	}
+	if !found
+		puts "#{card} not found. re enter:"
+		card.replace gets.chomp
+		return true
+	end
+}
+
 #parsing a suggest to form Suggestion. suggest should be a string of form "suggest_by w/r/s w/r/s w/r/s disproved_by card_shown". ie. weapon, room or suspect can be placed anywhere in 2->4 position
-#for now no error handling. assumed that suggest is of the correct form
-def parse_suggest(suggest) 
+def parse_suggest(suggest)
 	suggest = suggest.split
+	len = suggest.length
 	
+	error_handler{
+		if len < 4 or len >6
+			puts "re-enter suggest. not of valid format"
+			suggest = gets.chomp.split
+			len = suggest.length
+			true
+		end
+	}
+
+	new_suggest = Suggestions.new()
+	error_handler([[$all_players, new_suggest.suggest_by]], suggest[0], &$finder)
+
+	for i in 1..3
+		error_handler([[$all_weapons, new_suggest.weapon],[$all_rooms, new_suggest.room],[$all_suspects, new_suggest.suspect]], suggest[i], &$finder)
+	end
+	
+	error_handler([[$all_players, new_suggest.disproved_by]], suggest[4], &$finder) if len>=5
+
+	error_handler([[$all_cards, new_suggest.card_shown]], suggest[i], &$finder) if len==6
+
+	puts $all_suggestions
 end
 
 #algorithm 1: simplest one. in which you find out all the cards people have and the cards which arent there are in the envelope
 def algo1
 	
 end
+
+Players.new("sg", 4, 4)
+Players.new("meeks", 5, 2)
+Players.new("aly", 5, 1)
+Players.new("ali", 4, 3)
 
 #main body:
 found=false
